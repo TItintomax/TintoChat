87a12893daa4078f85eb911005ffde0ee96ea9d7
TItintomax <suporteti.tmx@gmail.com>
2023-12-12 10:15:09 -0300

    Alterar O app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+﻿from fastapi import FastAPI
 import TelaInicial
 from TelaInicial import main
 
 
 app = FastAPI()
 
-
+#modificando para UTF-8
 @app.get("/")
 def read_root():
     TelaInicial.ft.app(target=main, assets_dir="assets")
