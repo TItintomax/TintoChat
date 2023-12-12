38d13775a5d586c1ad97fe4f5883f16d5a4db0da
TItintomax <suporteti.tmx@gmail.com>
2023-12-12 13:59:36 -0300

    atualizar app.py para async

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 import TelaInicial
 from TelaInicial import main
+import micropip
 
+async def install_packages():
+    await micropip.install('anyio==3.7.1')
+    # Outras instalações necessárias
+
+install_packages()
 
 app = FastAPI()
 
