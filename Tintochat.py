e9a371517499aa56b81be03468fb042952d19525
TItintomax <153540729+TItintomax@users.noreply.github.com>
2023-12-13 09:01:59 -0300

    Update Tintochat.py

--- Tintochat.py
+++ Tintochat.py
@@ -2,10 +2,15 @@ import micropip
 import TelaInicial
 from TelaInicial import main
 import flet as ft
+import asyncio
 
 async def install_packages():
     await micropip.install("ssl")
-if __name__ == "__main__":
-    # Chame as funções diretamente
-    install_packages()
+
+async def main_async():
+    await install_packages()
+    # Inicialize sua aplicação aqui
     TelaInicial.ft.app(target=main, assets_dir="assets")
+
+# Executar o main_async em um loop de eventos
+asyncio.run(main_async())
