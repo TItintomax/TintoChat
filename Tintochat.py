1e43f9da18eaf194e4aad0473aed7fbb4a088ace
TItintomax <153540729+TItintomax@users.noreply.github.com>
2023-12-13 08:50:34 -0300

    Update Tintochat.py

--- Tintochat.py
+++ Tintochat.py
@@ -1,7 +1,10 @@
+import micropip
 import TelaInicial
 from TelaInicial import main
 import flet as ft
 if __name__ == "__main__":
     # Chame as funções diretamente
+    async def install_packages():
+        await micropip.install("ssl")
 
     TelaInicial.ft.app(target=main, assets_dir="assets")
